8dfd288a7591871cc7a0f391f71638a016afbe0e
Vitor Milam <vijmilam@gmail.com>
2026-09-10 19:08:57 +0000

    Changed app.py by importing flask, declaring host/port and adding 2 endpoints

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("Hello World")
+# Importing flask
+from flask import Flask
+
+app = Flask(__name__)
+
+# Endpoint
+@app.route("/")
+def hello():
+    return "Hello World"
+
+@app.route("/new")
+def hello():
+    return "It's a new world, a new day."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
